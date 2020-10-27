223a76fb5a49c882528e3135e79241b9e01a584d
amit <aamiiit@gmail.com>
2020-10-27 11:07:08 +0530

    adding argument

--- src/main/java/com/data/App.java
+++ src/main/java/com/data/App.java
@@ -5,6 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Eclips Jenkins");
+		System.out.println("Hello Amit...");
 	}
 
 }
